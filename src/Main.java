6a030b21b77263d994b98af9ef3808cee238eb20
iliandvl <iliandevlamijnck@gmail.com>
2023-07-17 14:50:14 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+    }
+    public static void emprunt(Library library){
+        Book book = cherche_livre(library);
+        if (book.available){
+
+        } else {
+            System.out.println("ce livre n'est pas disponible");
+        }
+    }
+    public static Book chercheLivre(Library library){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entrez un nom de livre");
+        String name = scanner.next();
+        System.out.println("Entrez un nom d'auteur'");
+        String autor = scanner.next();
+        for (Book book : library.bookList){
+            if (book.name == name && book.autor.name == autor){
+                Book wantedBook = book;
+            }
+        }
     }
 }
