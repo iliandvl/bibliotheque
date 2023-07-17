341cbef5e7c17a7f3cfb4132eb166749c4412ce0
iliandvl <iliandevlamijnck@gmail.com>
2023-07-17 15:10:56 +0200

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ public class Main {
     }
     public static void emprunt(Library library){
         Book book = cherche_livre(library);
-        if (book.available){
+        if (book != null){
+            if (book.available){
 
-        } else {
-            System.out.println("ce livre n'est pas disponible");
+            } else {
+                System.out.println("ce livre n'est pas disponible");
+            }
         }
     }
     public static Book chercheLivre(Library library){
@@ -17,10 +19,18 @@ public class Main {
         String name = scanner.next();
         System.out.println("Entrez un nom d'auteur'");
         String autor = scanner.next();
+        boolean bookFound = false;
         for (Book book : library.bookList){
             if (book.name == name && book.autor.name == autor){
                 Book wantedBook = book;
+                bookFound = true;
             }
+        }
+        if (!bookFound){
+            Book noBook = null;
+            return noBook;
+        } else {
+            return wantedBook;
         }
     }
 }
