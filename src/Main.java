0c369f7473301d3fd1d77564cb5b036c754ea94d
iliandvl <iliandevlamijnck@gmail.com>
2023-07-18 10:07:57 +0200

    update chercheLivre

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
     }
     public static void emprunt(Library library){
-        Book book = cherche_livre(library);
+        Book book = chercheLivre(library);
         if (book != null){
-            if (book.available){
+        if (book.isAvailable()){
                 library.borrowBook(book);
             } else {
                 System.out.println("ce livre n'est pas disponible");
@@ -16,15 +16,15 @@ public class Main {
     }
 
     public static void retour(Library library){
-        Book book = cherche_livre(library);
+        Book book = chercheLivre(library);
         if (book != null){
-            library.returnBook(book)
+            library.returnBook(book);
         }
     }
 
     public static void afficherListeLivres(Library library){
         for (Book book : library.bookList){
-            System.out.println(book.name + " : " + book.author.name);
+            System.out.println(book.getName() + " : " + book.getAutor());
         }
     }
     public static Book chercheLivre(Library library){
@@ -33,18 +33,16 @@ public class Main {
         String name = scanner.next();
         System.out.println("Entrez un nom d'auteur'");
         String author = scanner.next();
+
         boolean bookFound = false;
+        Book wantedBook = null;
         for (Book book : library.bookList){
-            if (book.name == name && book.author.name == author){
-                Book wantedBook = book;
+            if (book.getName() == name && book.getAutor() == author){
+                wantedBook = book;
                 bookFound = true;
             }
         }
-        if (!bookFound){
-            Book noBook = null;
-            return noBook;
-        } else {
-            return wantedBook;
-        }
+        return wantedBook;
+
     }
 }
