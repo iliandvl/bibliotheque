c24bac5db2eade9973563f2a40f4ea398854baf2
wilferoquendo <126185829+wilferoquendo@users.noreply.github.com>
2023-07-18 15:54:44 +0200

    THE END

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Library library = new Library("Test");
         library.addBook(new Book("Test","Test",new Author("Test"),new Author("Test"),new Author("Test")));
 
-        int response = 0;
+        int response;
         Scanner sc = new Scanner(System.in);
 
 
@@ -18,7 +18,8 @@ public class Main {
             System.out.println("2. Rendre un livre");
             System.out.println("3. Ajouter un livre dans la bibliothèque");
             System.out.println("4. Afficher tous les livres");
-            response = 0;
+            System.out.println("5. Supprimer un livre");
+//            response = 0;
             response = sc.nextInt();
             switch (response){
                 case 1:
@@ -28,10 +29,13 @@ public class Main {
                     retour(library);
                     break;
                 case 3:
-
+                    addBook();
                     break;
                 case 4:
                    afficherListeLivres(library);
+                    break;
+                case 5:
+                    deleteBook(library);
                     break;
                 case 0:
                     break;
@@ -81,7 +85,31 @@ public class Main {
         }
         return wantedBook;
     }
+   static void addBook(){
+       System.out.println("Entrez un nom de livre");
+       Scanner sc = new Scanner(System.in);
+       String title = sc.nextLine();
+       System.out.println("Entrez un nom d'auteur (Prenom nom)'");
+       String author = sc.nextLine();
+       Library library = new Library(title);
+       library.addBook(new Book(title,title,new Author(author)));
+       afficherListeLivres(library);
+   }
 
+   static void deleteBook(Library library) {
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Entrez un nom de livre");
+       String name = scanner.next();
+       System.out.println("Entrez un nom d'auteur (Prenom nom)'");
+       String author = scanner.next();
+       Book wantedBook = library.searchBook(name, author);
+       if (wantedBook == null){
+           System.out.println("livre introuvable");
+       } else {
+           library.removeBook(wantedBook);
+           System.out.println("livre suprimé");
+       }
 
+   }
 
 }
